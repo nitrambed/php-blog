a3bcf52743b3744e67efd0c8b1be0bd58bb13519
nitrambed <nitrambed@protonmail.com>
2021-10-11 11:20:15 +0200

    First Commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <script async src="public/js/index.js"></script>
+    <title>Blog</title>
+</head>
+
+<body>
+    <div class="container">
+        <?php require_once 'includes/header.php' ?>
+        <div class="content">
+        </div>
+        <?php require_once 'includes/footer.php' ?>
+    </div>
+</body>
+
+</html>
