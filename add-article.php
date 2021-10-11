1b3637cd3a18601dedbe29eeb7cd56f807cc57e7
nitrambed <nitrambed@protonmail.com>
2021-10-11 15:24:19 +0200

    Form structre and css

--- add-article.php
+++ add-article.php
@@ -13,10 +13,49 @@
     <title>Créer un article</title>
 </head>
 
+<!-- title -->
+<!-- img -->
+<!-- category -->
+<!-- content -->
+
 <body>
     <div class="container">
         <?php require_once 'includes/header.php' ?>
         <div class="content">
+            <div class="block p-20 form-container">
+                <h1>Écrire un article</h1>
+                <form action="/add-article.php" method='POST'>
+                    <div class="form-control">
+                        <label for="title">Titre</label>
+                        <input type="text" name="title" id="title">
+                        <!-- <p class="text-error"></p> -->
+                    </div>
+                    <div class="form-control">
+                        <label for="image">Image</label>
+                        <input type="text" name="image" id="image">
+                        <!-- <p class="text-error"></p> -->
+                    </div>
+                    <div class="form-control">
+                        <label for="category">Catégorie</label>
+                        <select name="category" id="category">
+                            <option value="technology">Technologie</option>
+                            <option value="nature">Nature</option>
+                            <option value="politics">Politique</option>
+                        </select>
+                        <!-- <p class="text-error"></p> -->
+                    </div>
+                    <div class="form-control">
+                        <label for="content">Contenu</label>
+                        <textarea name="content" id="content"></textarea>
+                        <!-- <p class="text-error"></p> -->
+                    </div>
+                    <div class="form-actions">
+                        <a href="/" class="btn btn-secondary">Annuler</a>
+                        <button class="btn btn-primary">Sauvegarder</button>
+
+                    </div>
+                </form>
+            </div>
         </div>
         <?php require_once 'includes/footer.php' ?>
     </div>
