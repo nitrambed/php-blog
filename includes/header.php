b07207bb7ed0d43d3a3caac1e7c7f6e47b2196f0
nitrambed <nitrambed@protonmail.com>
2021-10-15 10:52:24 +0200

    CRUD Blogpost

--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,8 @@
-<?php
-
-?>
-
 <header>
-    <a href="/" class="logo">Blog</a>
+    <a href="/" class="logo">Dyma Blog</a>
     <ul class="header-menu">
-        <li class=<?= $_SERVER['REQUEST_URI'] === '/add-article.php' ? 'active' : '' ?>>
-            <a href="/add-article.php">Écrire un article</a>
+        <li class=<?= $_SERVER['REQUEST_URI'] === '/form-article.php' ? 'active' : '' ?>>
+            <a href="/form-article.php">Écrire un article</a>
         </li>
     </ul>
 </header>
--- index.php
+++ index.php
@@ -1,14 +1,43 @@
+<?php
+$filename = __DIR__ . '/data/articles.json';
+$articles = [];
+$categories = [];
+
+$_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$selectedCat = $_GET['cat'] ?? '';
+
+if (file_exists($filename)) {
+    $articles = json_decode(file_get_contents($filename), true) ?? [];
+    $cattmp = array_map(fn ($a) => $a['category'],  $articles);
+    $categories = array_reduce($cattmp, function ($acc, $cat) {
+        if (isset($acc[$cat])) {
+            $acc[$cat]++;
+        } else {
+            $acc[$cat] = 1;
+        }
+        return $acc;
+    }, []);
+    $articlePerCategories = array_reduce($articles, function ($acc, $article) {
+        if (isset($acc[$article['category']])) {
+            $acc[$article['category']] = [...$acc[$article['category']], $article];
+        } else {
+            $acc[$article['category']] = [$article];
+        }
+        return $acc;
+    }, []);
+}
+
+
+?>
+
+
+
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/css/style.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
-    <script async src="public/js/index.js"></script>
+    <?php require_once 'includes/head.php' ?>
+    <link rel="stylesheet" href="/public/css/index.css">
     <title>Blog</title>
 </head>
 
@@ -16,9 +45,47 @@
     <div class="container">
         <?php require_once 'includes/header.php' ?>
         <div class="content">
+            <div class="newsfeed-container">
+                <ul class="category-container">
+                    <li class=<?= $selectedCat ? '' : 'cat-active' ?>><a href="/">Tous les articles <span class="small">(<?= count($articles) ?>)</span></a></li>
+                    <?php foreach ($categories as $catName => $catNum) : ?>
+                        <li class=<?= $selectedCat ===  $catName ? 'cat-active' : '' ?>><a href="/?cat=<?= $catName ?>"> <?= $catName ?><span class="small">(<?= $catNum ?>)</span> </a></li>
+                    <?php endforeach; ?>
+                </ul>
+                <div class="newsfeed-content">
+                    <?php if (!$selectedCat) : ?>
+                        <?php foreach ($categories as $cat => $num) : ?>
+                            <h2><?= $cat ?></h2>
+                            <div class="articles-container">
+                                <?php foreach ($articlePerCategories[$cat] as $a) : ?>
+                                    <a href="/show-article.php?id=<?= $a['id'] ?>" class="article block">
+                                        <div class="overflow">
+                                            <div class="img-container" style="background-image:url(<?= $a['image'] ?>"></div>
+                                        </div>
+                                        <h3><?= $a['title'] ?></h3>
+                                    </a>
+                                <?php endforeach; ?>
+                            </div>
+                        <?php endforeach; ?>
+                    <?php else : ?>
+                        <h2><?= $selectedCat ?></h2>
+                        <div class="articles-container">
+                            <?php foreach ($articlePerCategories[$selectedCat] as $a) : ?>
+                                <a href="/show-article.php?id=<?= $a['id'] ?>" class="article block">
+                                    <div class="overflow">
+                                        <div class="img-container" style="background-image:url(<?= $a['image'] ?>"></div>
+                                    </div>
+                                    <h3><?= $a['title'] ?></h3>
+                                </a>
+                            <?php endforeach; ?>
+                        </div>
+                    <?php endif; ?>
+                </div>
+            </div>
         </div>
         <?php require_once 'includes/footer.php' ?>
     </div>
+
 </body>
 
 </html>
